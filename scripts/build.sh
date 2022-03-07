2989e757cfa3a0abd878139f7745a6f7e6c3488c
artiumex <70172435+artiumex@users.noreply.github.com>
2022-03-07 15:21:17 +0000

    Initial commit

---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+node tiddlywiki/tiddlywiki.js gigecon --build index
+
+cp -r gigecon/output docs 
---
+++ scripts/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+node tiddlywiki/tiddlywiki.js gigecon --listen port=3000 host=0.0.0.0
